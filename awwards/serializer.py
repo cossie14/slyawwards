bb6167974169396c091469f8500f8b7e79cba2d4
sylviah <sylviah.rutto@gmail.com>
2019-09-06 16:37:39 +0300

    configure api

--- awwards/serializer.py
+++ awwards/serializer.py
@@ -4,9 +4,9 @@ from .models import Profile,Project
 class AwardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('id', 'title', 'post', 'pub_date','project_pic')
+        fields = ('id', 'title', 'project_posts', 'project_url','project_pic','pub_date','project_pic')
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('id', 'username', 'contact', 'bio','profile_picture','user_id')
+        fields = ( 'username', 'contact', 'bio','profile_picture','user_id')
