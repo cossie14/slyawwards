86a696fdcf5ace57230cdf06f0272ff672ac6e3f
sylviah <sylviah.rutto@gmail.com>
2019-09-07 18:48:24 +0300

    added css star rating

--- awwards/views.py
+++ awwards/views.py
@@ -181,12 +181,12 @@ def newsletter(request):
 
 def search_results(request):
 
-    if 'article' in request.GET and request.GET["article"]:
-        search_term = request.GET.get("article")
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
         searched_articles = Article.search_by_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"articles": searched_articles})
+        return render(request, 'search.html',{"message":message,"": searched_articles})
 
     else:
         message = "You haven't searched for any term"
